0f50d629b599079689ff53552b5b1cf34b17a834
lanebo1 <k.efimovich@innopolis.university>
2024-10-06 22:08:48 +0300

    .

--- src/main.test.ts
+++ src/main.test.ts
@@ -3,7 +3,7 @@ import { expect, test } from "bun:test";
 import { maximize } from "./main";
 
 test("Sample test", () => {
-  const left = maximize([1, 2, 3], [{ coeffs: [1, 2, 3], rhs: 2 }]);
+  const left = maximize([9, 10, 16], [{ coeffs: [18, 15, 12], rhs: 360 }, {coeffs: [6, 4, 8], rhs: 192}, {coeffs: [5, 3, 3], rhs: 180}]);
   const right = 42;
 
   expect(left).toBe(right);
