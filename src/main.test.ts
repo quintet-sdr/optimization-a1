9867f73815e9829d3424e573db33ba68a6d60771
Asqar Arslanov <asqarslanov@gmail.com>
2024-10-06 21:34:51 +0300

    .

--- src/main.test.ts
+++ src/main.test.ts
@@ -3,5 +3,8 @@ import { expect, test } from "bun:test";
 import { maximize } from "./main";
 
 test("Sample test", () => {
-  expect(maximize([1, 2, 3], true)).toBe(42);
+  const left = maximize([1, 2, 3], [{ coeffs: [1, 2, 3], rhs: 2 }]);
+  const right = 42;
+
+  expect(left).toBe(right);
 });
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,39 @@
-export function maximize(coefficients: number[], log: boolean): number {
+type Constraint = {
+  coeffs: number[];
+  rhs: number;
+};
+
+export function maximize(coeffs: number[], constraints: Constraint[]): number {
+  console.log("Starting the simplex algorithm.");
+  console.log(`Function: ${coeffsToFn(coeffs)}`);
+
+  constraints.forEach((c, i) => {
+    if (c.coeffs.length !== coeffs.length) {
+      throw new Error(
+        `numbers of coefficients don't match:\n` +
+          ` function: ${coeffs} (${coeffs.length})\n` +
+          ` constraint ${i + 1}: ${c.coeffs} (${c.coeffs.length})`
+      );
+    }
+  });
+
+  const table = arrayOf(
+    constraints.length + 1,
+    // arrayOf(, 0)
+    0 // FIXME
+  );
+
+  // TODO: algorithm
+
+  console.log(table);
+
   return 42;
 }
 
-function main(): void {
-  console.log("Hello via Bun!");
+function arrayOf<T>(n: number, item: T): T[] {
+  return new Array(n).fill(item);
 }
 
-if (import.meta.main) {
-  main();
+function coeffsToFn(coeffs: number[]): string {
+  return coeffs.map((it, i) => `${it}*x[${i + 1}]`).join(" + ");
 }
